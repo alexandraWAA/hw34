4b427805cd6e9c9d3d4a12727494a66e7df385cd
Aleksandra Volchikhina <aleksandravolcihina@gmail.com>
2026-06-17 01:14:42 +0300

    1st commit

--- lms/tasks.py
+++ lms/tasks.py
@@ -5,7 +5,10 @@ from django.conf import settings
 from django.utils import timezone
 from celery import shared_task
 from celery.utils.log import get_task_logger
-from lms.models import Course, Lesson, Subscription
+
+# Добавляем импорт модели User
+from users.models import User
+from lms.models import Course, Subscription
 
 logger = get_task_logger(__name__)
 
@@ -40,6 +43,14 @@ def send_course_update_notification(course_id, updated_fields):
 
 @shared_task
 def deactivate_inactive_users():
+    """Деактивация пользователей, не заходивших более 30 дней"""
     thirty_days_ago = timezone.now() - timedelta(days=30)
-    updated_count = User.objects.filter(last_login__lt=thirty_days_ago, is_active=True, is_superuser=False).update(is_active=False)
+    updated_count = User.objects.filter(
+        last_login__lt=thirty_days_ago,
+        is_active=True,
+        is_superuser=False
+    ).update(is_active=False)
+
+    if updated_count > 0:
+        logger.info('Деактивировано %d неактивных пользователей', updated_count)
     return {'deactivated': updated_count}
